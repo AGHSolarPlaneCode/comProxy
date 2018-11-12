68fdb34f6133e9d9accef69c01f931dfca2ca03f
Hubertus248 <hubertus248@gmail.com>
2018-11-12 16:07:59 +0100

    Parser tests

--- main.go
+++ main.go
@@ -6,17 +6,19 @@ import (
 	"os"
 	"strconv"
 	"io"
+	"github.com/ungerik/go-mavlink"
 )
 
 func main() {
 	if len(os.Args) < 3 {
-		println("Usage: comProxy port1 port2 baudRate")
+		//println("Usage: comProxy port1 port2 baudRate")
+		println("Usage: comProxy portName baudRate")
 		return
 	}
 
 	port1Name := os.Args[1]
-	port2Name := os.Args[2]
-	baudRate, err := strconv.ParseUint(os.Args[3], 10, 32)
+	//port2Name := os.Args[2]
+	baudRate, err := strconv.ParseUint(os.Args[2], 10, 32)
 
 	if err != nil {
 		log.Fatal(err)
@@ -30,13 +32,13 @@ func main() {
 		MinimumReadSize: 4,
 	}
 
-	options2 := serial.OpenOptions{
-		PortName:        port2Name,
-		BaudRate:        uint(baudRate),
-		DataBits:        8,
-		StopBits:        1,
-		MinimumReadSize: 4,
-	}
+	//options2 := serial.OpenOptions{
+	//	PortName:        port2Name,
+	//	BaudRate:        uint(baudRate),
+	//	DataBits:        8,
+	//	StopBits:        1,
+	//	MinimumReadSize: 4,
+	//}
 
 	port1, err := serial.Open(options1)
 
@@ -46,30 +48,51 @@ func main() {
 
 	defer port1.Close()
 
-	port2, err := serial.Open(options2)
+	//port2, err := serial.Open(options2)
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//defer port2.Close()
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	parser := mavlink.GetMavParser()
 
-	defer port2.Close()
+	bytes := make([]byte, 1)
 
-	bytes := make([]byte, 32)
+	//for {
+	//	n := read(port1, &bytes)
+	//	if n > 0 {
+	//		write(port2, &bytes, n)
+	//	}
+	//
+	//	n = read(port2, &bytes)
+	//	if n > 0 {
+	//		write(port1, &bytes, n)
+	//	}
+	//}
 
 	for {
 		n := read(port1, &bytes)
 		if n > 0 {
-			write(port2, &bytes, n)
-		}
+			for _, b := range bytes[:n] {
+				packet, err := parser(b)
+				if err != nil {
+					log.Fatal(err)
 
-		n = read(port2, &bytes)
-		if n > 0 {
-			write(port1, &bytes, n)
+				} else if packet != nil {
+					processPacket(packet)
+				}
+			}
 		}
 	}
 }
 
-func  read(port io.ReadWriteCloser, bytes *[]byte) int {
+func processPacket(packet *mavlink.MavPacket) {
+	println(packet.Header.MessageID)
+}
+
+func read(port io.ReadWriteCloser, bytes *[]byte) int {
 	n, err := port.Read(*bytes)
 
 	if err != nil {
